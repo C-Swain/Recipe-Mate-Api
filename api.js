91f0600974ef24649fad7a382e5c53712d8d9208
Catherine Swain <catherinemexicoswain@gmail.com>
2021-11-19 12:17:55 -0500

    initial commit

---
+++ api.js
@@ -0,0 +1,68 @@
+const inMemoryRecipes = [
+  {
+    id: 123,
+    user_id: 456,
+    name: "toast toast",
+    category: "breakfast",
+    description: "very tasty",
+    steps: "put in toaster",
+    servings: "1 person",
+    time: 5,
+    likes: 3
+  },
+  {
+    id: 124,
+    user_id: 457,
+    name: "cereal",
+    category: "breakfast",
+    description: "easy food",
+    steps: "get bowl, pour cereal , add milk . do not put in toaster",
+    servings: "1 person",
+    time: 5,
+    likes: 3
+  },
+  {
+    id: 129,
+    user_id: 356,
+    name: "Peanut-butter and jam sandwich",
+    category: "breakfast",
+    description: "very tasty",
+    steps: "peanut butter jelly time",
+    servings: "100 people",
+    time: 5,
+    likes: 3
+  }
+];
+
+const getAllRecipes = async (request, response) => {
+  response.status(200).json(inMemoryRecipes);
+};
+
+const getRecipeById = (request, response) => {
+  response.status(200).json(inMemoryRecipes[0]);
+};
+
+const addRecipe = async (request, response) => {
+  const { name, rating } = request.body;
+  inMemoryRecipes.push({ name, rating });
+  response.status(201).send(`Recipe added successfully.`);
+};
+
+const updateRecipe = (request, response) => {
+  const { name, rating } = request.body;
+  inMemoryRecipes[0] = { name, rating };
+  response.status(200).send(`First Recipe in list is updated.`);
+};
+
+const deleteRecipe = (request, response) => {
+  inMemoryRecipes.shift();
+  response.status(200).send(`First Recipe in list is deleted.`);
+};
+
+module.exports = {
+  getAllRecipes,
+  getRecipeById,
+  addRecipe,
+  updateRecipe,
+  deleteRecipe
+};
---
+++ app.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+const api = require('./api');
+// const { getAllRecipes,getRecipeById, addRecipe,updateRecipe, deleteRecipe} from a[]
+
+app.get('/Recipes/', api.getAllRecipes);
+app.get('/Recipes/:id', api.getRecipeById);
+app.post('/Recipes/', api.addRecipe);
+app.put('/Recipes/:id', api.updateRecipe);
+app.delete('/Recipes/:id', api.deleteRecipe);
+
+app.listen(port, () => {
+  console.log(` Recipe-Mate Api is at your service ${port}.`);
+});
