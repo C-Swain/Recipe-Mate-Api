850226af6b19d882034c0d277c1b8593a957111f
Catherine Swain <catherinemexicoswain@gmail.com>
2021-11-19 16:43:53 -0500

    tables and seeds functional

--- src/db/schema/seeds.sql
+++ src/db/schema/seeds.sql
@@ -24,13 +24,13 @@ VALUES ('Breakfast', True),
 -- );
 
 INSERT INTO recipes (user_id, name, category, description, ingredients, steps, servings, time, likes)
-VALUES (1, "Vegan Fish and Chips", 3,"This is the best Vegan Fish","Celeriac, batter", "1: Slice up Celeriac. 2: Bake in oven on 200 until soft 3:fry", "2 people", 60, 140),
-(2, "Cereal", 1, "a breakfast favourite", "Cereal, Milk", "1: Pour your favourite cereal into a bowl, 2: add milk 3: Finnish with spoon","1 person", 1, 54);
+VALUES (1, 'Vegan Fish and Chips', 3, 'This is the best Vegan Fish','Celeriac, batter', '1: Slice up Celeriac. 2: Bake in oven on 200 until soft 3:fry', '2 people', 60, 140),
+(2, 'Cereal', 1, 'a breakfast favourite', 'Cereal, Milk', '1: Pour your favourite cereal into a bowl, 2: add milk 3: Finnish with spoon','1 person', 1, 54);
 
 INSERT INTO comments (recipe_id, user_id, description)
-VALUES (1, 3, "I love this ! it's so good <3"),
-(2, 2, "recipe portion too small make it double, or you will be hungry!"),
-(1, 1, "I do not like.. it takes like socks");
+VALUES (1, 3, 'I love this ! it is so good <3'),
+(2, 2, 'recipe portion too small make it double, or you will be hungry!'),
+(1, 1, 'I do not like.. it takes like socks');
 
 INSERT INTO favourites (user_id, recipe_id)
 VALUES (1, 1),
