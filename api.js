b4de43f96b443a1e24e7ef3b601f416be6da6f28
Catherine Swain <catherinemexicoswain@gmail.com>
2021-11-19 21:21:34 -0500

    added users paths

--- api.js
+++ api.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const { Pool } = require("pg");
 const pool = new Pool({
   user: 'development',
@@ -6,44 +7,44 @@ const pool = new Pool({
   database: 'recipe_development'
 });
 
-const inMemoryRecipes = [
-  {
-    id: 123,
-    user_id: 456,
-    name: 'toast toast',
-    category: 'breakfast',
-    description: 'very tasty',
-    ingredients: 'bread',
-    steps: 'put in toaster',
-    servings: '1 person',
-    time: 5,
-    likes: 3
-  },
-  {
-    id: 124,
-    user_id: 457,
-    name: 'cereal',
-    category: 'breakfast',
-    description: 'easy food',
-    ingredients: ' cereal , milk',
-    steps: 'get bowl, pour cereal , add milk . do not put in toaster',
-    servings: '1 person',
-    time: 5,
-    likes: 3
-  },
-  {
-    id: 129,
-    user_id: 356,
-    name: 'Peanut-butter and jam sandwich',
-    category: 'breakfast',
-    description: 'very tasty',
-    ingredients: ' bread, peanut-butter , jam',
-    steps: 'peanut butter jelly time',
-    servings: '100 people',
-    time: 5,
-    likes: 3
-  }
-];
+// const inMemoryRecipes = [
+//   {
+//     id: 123,
+//     user_id: 456,
+//     name: 'toast toast',
+//     category: 'breakfast',
+//     description: 'very tasty',
+//     ingredients: 'bread',
+//     steps: 'put in toaster',
+//     servings: '1 person',
+//     time: 5,
+//     likes: 3
+//   },
+//   {
+//     id: 124,
+//     user_id: 457,
+//     name: 'cereal',
+//     category: 'breakfast',
+//     description: 'easy food',
+//     ingredients: ' cereal , milk',
+//     steps: 'get bowl, pour cereal , add milk . do not put in toaster',
+//     servings: '1 person',
+//     time: 5,
+//     likes: 3
+//   },
+//   {
+//     id: 129,
+//     user_id: 356,
+//     name: 'Peanut-butter and jam sandwich',
+//     category: 'breakfast',
+//     description: 'very tasty',
+//     ingredients: ' bread, peanut-butter , jam',
+//     steps: 'peanut butter jelly time',
+//     servings: '100 people',
+//     time: 5,
+//     likes: 3
+//   }
+// ];
 
 const getAllRecipes = async (request, response) => {
     pool.query('SELECT * FROM Recipes;', (error, results) => {
@@ -60,27 +61,68 @@ const getRecipeById = (request, response) => {
 };
 
 const addRecipe = async (request, response) => {
-  const { name, rating } = request.body;
-  pool.query('INSERT INTO recipes (name, rating) VALUES ($1, $2)', [name, rating], (error, results) => {
+  const { user_id, name, category, description, ingredients, steps, servings, time} = request.body;
+  pool.query('INSERT INTO recipes (user_id, name, category, description, ingredients, steps, servings, time) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [ user_id, name, category, description, ingredients, steps, servings, time], (error, results) => {
     response.status(201).send(`Recipe added successfully.`);
   });
 };
 
-const updateRecipe = (request, response) => {
-  const { name, rating } = request.body;
-  inMemoryRecipes[0] = { name, rating };
-  response.status(200).send(`First Recipe in list is updated.`);
-};
+// const updateRecipe = (request, response) => {
+//   const id = parseInt(request.params.id);
+//   const { name, rating } = request.body;
+//   pool.query(
+//     'UPDATE recipes SET name = $1, rating = $2 WHERE id = $3', [user_id, name, category, description, ingredients, steps, servings, time], (error, results) => {
+//       response.status(200).send(`recipe with id ${id} modified.`);
+//     }
+//   );
+// };
 
 const deleteRecipe = (request, response) => {
-  inMemoryRecipes.shift();
-  response.status(200).send(`First Recipe in list is deleted.`);
+  const id = parseInt(request.params.id);
+  pool.query('DELETE FROM recipe WHERE id = $1', [id], (error, results) => {
+    response.status(200).send(`recipe with id ${id} deleted.`);
+  });
+};  
+
+
+// Users Paths
+const getAllUsers = async (request, response) => {
+    pool.query('SELECT * FROM Users;', (error, results) => {
+    response.status(200).json(results.rows);
+    });
 };
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    response.status(200).json(results.rows);
+  });
+};
+
+const addUser = async (request, response) => {
+  const {email ,password} = request.body;
+  pool.query('INSERT INTO users (email, password) VALUES ($1, $2)', [email, password], (error, results) => {
+    response.status(201).send(`user added successfully.`);
+  });
+};
+
+//Favourites Not working
+// const getFavouritesByUser = async (request, response) => {
+//   const user = parseInt(request.params.user);
+//   pool.query('SELECT * FROM favourites WHERE user_id = $1', [user], (error, results) => {
+//     response.status(200).json(results.rows);
+//   });
+// };
+
 
 module.exports = {
   getAllRecipes,
   getRecipeById,
   addRecipe,
-  updateRecipe,
-  deleteRecipe
+  // updateRecipe,
+  deleteRecipe,
+  getAllUsers,
+  getUserById,
+  addUser,
+  getFavouritesByUser
 };
--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const port = 5000;
 app.use(express.json());
 
 const api = require('./api');
-
+//recipes paths 
 app.get('/Recipes/', api.getAllRecipes);
 app.get('/Recipes/:id', api.getRecipeById);
 app.post('/Recipes/', api.addRecipe);
-app.put('/Recipes/:id', api.updateRecipe);
+// app.put('/Recipes/:id', api.updateRecipe);
 app.delete('/Recipes/:id', api.deleteRecipe);
+
+//users paths
+app.get('/Users/', api.getAllUsers);
+app.get('/Users/:id', api.getUserById);
+app.post('/Users/', api.addUser);
+
+//favourites
+app.get('/Favourites/:userId', api.getFavouritesByUser);
+
 
 app.listen(port, () => {
   console.log(` Recipe-Mate Api is at your service ${port}.`);
